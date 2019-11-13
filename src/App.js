a6cd6026fa74943bc0ab76c0cadb61d97a6d502f
Miley Wright <dishaliekwright@yahoo.com>
2019-11-13 08:35:08 -0500

    added the productContext & cartContext Provider for add & removing products in App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//Context
+import ProductContext from './contexts/ProductContext';
+	//now that we created our ProductContext we can provide data across our app
+import CartContext from './contexts/CartContext';
+	// we can now provide data across our app
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -23,26 +29,34 @@ function App() {
 	}
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value = {{products, addItem}}>
+			{/* 1. now that we created our ProductContext, wrap all our components/routes. 
+			2. Pass a value props to the Provider (the products state and an addItem function that will allow us to add the product to the cart)*/}
+			<CartContext.Provider value = {{cart, removeItem}}>
+				{/* 1. now that we create our CartContext, wrap all components/routes.
+				2. Pass a value props to the Provider (the cart state and the removeItem function that will allow us to remove the product from the cart) */}
+				<div className="App">
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						render={() => (
+							<Products
+								products={products}
+								addItem={addItem}
+							/>
+						)}
 					/>
-				)}
-			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 

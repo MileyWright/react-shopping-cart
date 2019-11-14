56a4f1eba72c9c971fae4e0d6909bf80275add6f
Miley Wright <dishaliekwright@yahoo.com>
2019-11-13 20:31:27 -0500

    removeItem stretch completed

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,7 +6,7 @@ import Product from './Product';
 //Context
 import {ProductContext} from '../contexts/ProductContext';
 
-const Products = (s) => {
+const Products = () => {
 	const { products, addItem } = useContext(ProductContext);
 	// call on the useContext hook to pass in the context object that we want to use (ProductContext) into it. It's going to return the value prop passed by our ProductContext.Provider. (products & addItem)
 	// now that we have all of the data we can refactor our Products component from using props by removing every prop
